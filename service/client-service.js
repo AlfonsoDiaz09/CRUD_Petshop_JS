47d47e827c85d262426de6b97808dd452af395d2
AlfonsoDiaz09 <alfonsodiazc.09@gmail.com>
2023-09-26 08:37:58 -0600

    Registro de Clientes en archivo JSON Se realiza la función para el registro de clientes en el archivo .json y se hace uso de la libreria uuid que permite generar un id aleatorio

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,17 @@
 // Opción en una línea
 const listaClientes = () => fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json());
 
+const crearCliente = (nombre, email) => {
+    return fetch("http://localhost:3000/perfil", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nombre, email, id: uuid.v4()})
+    });
+};
+
 export const clientServices = {
     listaClientes,
+    crearCliente,
 };
